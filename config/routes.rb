9b2dbe3f6158e763189957a47869107664248f99
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-03-02 03:02:16 -0600

    Add 'show' route for articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # This is the index page of our app
   root 'pages#home'
 
+  # assing a route using a controller
   get 'about', to: 'pages#about'
+
+  resources :articles, only: [:show]
 
 end
