def87afcbbf4036ccf565a89093f656b9acac0cc
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-03-19 03:40:31 -0600

    Apply the DRY principle on article_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successufully"
       redirect_to @article # Here we redirect to the 'show' action, to see the new article information we add.
@@ -24,12 +24,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated succesfully"
       redirect_to @article # Here we redirect to the 'show' action, to see the new article information we add.
     else
@@ -38,9 +36,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path # This means we are redirecting to the articles index, showing all the articles we have.
+  end
+
+  private #--------------------------------------------------------------------------------------------------
+
+  # We call this method before :show, :edit, :update, destroy
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
