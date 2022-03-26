7a86d664ebd301d4e0a706adaf643211c14cf750
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-03-25 23:07:32 -0600

    Disable the div classes every time we have errors on the form

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# To disable form inputs with the html class of 'fields_with_errors'. This class appears everytime we have errors in our inputs and Rails added automatically.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
