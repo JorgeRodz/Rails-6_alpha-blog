1f84dac26a397b983f1cbac25bab53922e6beaaa
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-04-12 07:40:01 -0500

    Add sessions routes
    We create the sessions routes for new, create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,22 @@ Rails.application.routes.draw do
   # assing a route using a controller
   get 'about', to: 'pages#about'
 
-  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  resources :articles # This line is the same as above providing the REST pattern -> Representation State Transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions just like the line above. This is also provides REST-ful routes...this means we can acces this values like an API RESTFUl providing a JSON response and working the frontend with other tecnologies like React, Angular, Vue, etc. In this case we just want to work with the Rails Fullstack.
+  # ---------- articles routes ----------
+
+  # resources :articles = provide all of this routes [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles # This line are providing the REST pattern -> Representation State Transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions just like the line above. This is also provides REST-ful routes...this means we can acces this values like an API RESTFUl providing a JSON response and working the frontend with other tecnologies like React, Angular, Vue, etc. In this case we just want to work with the Rails Fullstack.
+
+
+  # ---------- users routes ----------
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+
+  # ---------- session routes ----------
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 end
