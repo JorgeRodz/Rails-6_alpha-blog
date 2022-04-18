dec7ea313fd32ef13ffc43f4ce0ce6162ce27954
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-04-18 18:09:33 -0500

    Add authentications helpers to application_controller
    This methods we also added to application_helper using helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in? # To make this helper method avalible on the view helpers
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
