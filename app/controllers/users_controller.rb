f9dc0da6e29d86ba0f2e62c0286cd8835a5a9d26
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-04-20 15:58:30 -0500

    Fix restriction with :new, :create
    Now we can create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   # restrictions
-  before_action :require_login_user, except: [:show, :index]
+  before_action :require_login_user, except: [:show, :index, :new, :create]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
