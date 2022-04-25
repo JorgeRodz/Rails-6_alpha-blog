c297b3e47020951f13d21d9460068b0109e04eed
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-04-25 17:12:37 -0500

    Add User Admin privilage
    User admin privilage to edit, delete articles and delete users.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:alert] = "You can only edit and delete article's of your own"
       redirect_to @article
     end
