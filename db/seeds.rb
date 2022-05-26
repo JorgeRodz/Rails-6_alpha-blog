f2024e3b49843c48fdca47d76e7f99ada44605c4
Jorge Rodriguez <jarf4321@gmail.com>
2022-05-25 19:24:53 -0500

    Add users on db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['leon', 'andres', 'natalia', 'camilo', 'rusbel', 'johan'].each do |name|
+	User.create email: "#{name}@gmail.com.com", username: name,password: "#{name}123"
+end
+
+puts 'Users has been created'
