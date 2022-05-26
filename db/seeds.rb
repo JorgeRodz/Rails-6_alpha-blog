c0be2498d2f5aa37288cec660a760699e53cf5bb
Jorge Rodriguez <jarf4321@gmail.com>
2022-05-25 19:42:59 -0500

    Add users & categories on db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create email: "jarf4321@gmail.com", username: "jarf", password: "jorge123", admin: true
+
 ['leon', 'andres', 'natalia', 'camilo', 'rusbel', 'johan'].each do |name|
-	User.create email: "#{name}@gmail.com.com", username: name,password: "#{name}123"
+	User.create email: "#{name}@gmail.com", username: name, password: "#{name}123"
 end
 
 puts 'Users has been created'
+
+['Restaurant', 'Sports', 'Travel', 'Cars', 'Tech', 'Science'].each do |name|
+	Category.create name: name
+end
+
+puts 'Categories has been created'
+
